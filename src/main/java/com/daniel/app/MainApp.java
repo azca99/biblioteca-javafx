1540d361ef76e09fb020bdefe9a4d43e31ea159a
azca99 <azca99@gmail.com>
2026-01-14 14:32:00 +0000

    feat: añade splash screen al inicio (#3)

--- src/main/java/com/daniel/app/MainApp.java
+++ src/main/java/com/daniel/app/MainApp.java
@@ -1,40 +1,74 @@
 package com.daniel.app;
 
+import javafx.animation.PauseTransition;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.ProgressIndicator;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+import javafx.util.Duration;
 import com.daniel.ui.PrestamosView;
 import com.daniel.ui.SociosView;
 import com.daniel.ui.LibrosView;
 import javafx.application.Application;
-import javafx.scene.Scene;
 import javafx.scene.control.Tab;
 import javafx.scene.control.TabPane;
-import javafx.stage.Stage;
 
 public class MainApp extends Application {
 
     @Override
     public void start(Stage stage) {
 
-        SociosView sociosView = new SociosView();
-        LibrosView librosView = new LibrosView();
-        PrestamosView prestamosView = new PrestamosView();
+        // Crear una ventana (Stage) aparte para el Splash, sin bordes
+        Stage splash = new Stage(StageStyle.UNDECORATED);
 
-        TabPane tabs = new TabPane();
+        // Crear los elementos visuales del Splash
+        Label lbl = new Label("Cargando Biblioteca...");
+        ProgressIndicator pi = new ProgressIndicator();
+        pi.setPrefSize(50, 50);
 
-        Tab tabSocios = new Tab("Socios", sociosView.createContent());
-        tabSocios.setClosable(false);
+        // Colocarlos en un contenedor vertical (VBox)
+        VBox box = new VBox(12, lbl, pi);
+        box.setAlignment(Pos.CENTER);
+        box.setPadding(new Insets(20));
 
-        Tab tabLibros = new Tab("Libros", librosView.createContent());
-        tabLibros.setClosable(false);
+        // Crear una escena para el Splash y la mostrarla
+        Scene splashScene = new Scene(box, 260, 140);
+        splash.setScene(splashScene);
+        splash.centerOnScreen();
+        splash.show();
 
-        Tab tabPrestamos = new Tab("Préstamos", prestamosView.createContent());
-        tabPrestamos.setClosable(false);
+        PauseTransition espera = new PauseTransition(Duration.seconds(1.2));
+        espera.setOnFinished(ev -> {
 
-        tabs.getTabs().addAll(tabSocios, tabLibros, tabPrestamos);
+            SociosView sociosView = new SociosView();
+            LibrosView librosView = new LibrosView();
+            PrestamosView prestamosView = new PrestamosView();
 
-        Scene scene = new Scene(tabs, 900, 500);
+            TabPane tabs = new TabPane();
 
-        stage.setTitle("Biblioteca");
-        stage.setScene(scene);
-        stage.show();
+            Tab tabSocios = new Tab("Socios", sociosView.createContent());
+            tabSocios.setClosable(false);
+
+            Tab tabLibros = new Tab("Libros", librosView.createContent());
+            tabLibros.setClosable(false);
+
+            Tab tabPrestamos = new Tab("Préstamos", prestamosView.createContent());
+            tabPrestamos.setClosable(false);
+
+            tabs.getTabs().addAll(tabSocios, tabLibros, tabPrestamos);
+
+            Scene scene = new Scene(tabs, 900, 500);
+
+            stage.setTitle("Biblioteca");
+            stage.setScene(scene);
+            splash.close();
+            stage.centerOnScreen();
+            stage.show();
+        });
+    espera.play();
     }
 }
